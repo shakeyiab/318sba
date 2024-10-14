11deb23acda35366803f2baf0d740de32ec8ca05
shakeyiab <shakeyiabartell@gmail.com>
2024-10-14 15:17:44 -0400

    changs project

--- server.mjs
+++ server.mjs
@@ -1,11 +1,12 @@
 // Imports
 import express from 'express';
-
-
+import userroutes from './routes/userroutes.mjs';
+import fs from 'fs';
 import bodyParser from 'body-parser';
 import loggr from './data/logger.mjs';
 import auth from './data/auth.mjs';
 
+import path from "path"
 
 // Create an instance of express
 const app = express();
@@ -15,10 +16,20 @@ let PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app.set('view engine','ejs');
+
+  
+app.set('views', './views')
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.render('base',{title:"login"});
-});
+    res.render('home', {
+      title: 'Search Hacker News',
+    });
+  });
+  
+  
+  
+  app.use('/', userroutes);
 
 // Listen
 app.listen(PORT, () => {
